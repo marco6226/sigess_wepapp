ed924f77da7e1c26e1d56f8e14546b38e0663141
unknown <enfranly.gucoma@gmail.com>
2020-10-19 10:05:08 -0500

    major fix to scm

--- src/app/modulos/scm/components/scm/scm.component.ts
+++ src/app/modulos/scm/components/scm/scm.component.ts
@@ -34,7 +34,7 @@ export class ScmComponent implements OnInit {
         'ultimoLogin',
         'ipPermitida',
         'mfa',
-        'numeroMovil'
+        'numeroMovil' 
         //'usuarioEmpresaList_perfil_id'
     ];
     estadosList: SelectItem[] = [
