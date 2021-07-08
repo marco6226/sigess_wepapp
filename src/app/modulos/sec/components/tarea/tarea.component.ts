682a86bd7369bf011c0e35fc138a3139e3bf3724
Marco Acero <marco_haz24@hotmail.com>
2021-07-08 13:12:13 -0500

    ajuste visual tareas

--- src/app/modulos/sec/components/tarea/tarea.component.ts
+++ src/app/modulos/sec/components/tarea/tarea.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { TareaService } from '../../services/tarea.service';
+import {
+    locale_es,
+    tipo_identificacion,
+    tipo_vinculacion,
+} from "app/modulos/rai/enumeraciones/reporte-enumeraciones";
 
 @Component({
     selector: 'app-tarea',
@@ -13,6 +18,9 @@ export class TareaComponent implements OnInit {
     tareaForm: FormGroup;
     routeSub;
     tarea: any;
+    fechaActual = new Date();
+    yearRange: string = "1900:" + this.fechaActual.getFullYear();
+    localeES: any = locale_es;
     constructor(fb: FormBuilder,
         private route: ActivatedRoute,
         private tareaService: TareaService,
