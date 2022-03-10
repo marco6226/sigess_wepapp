79953259e690c93a2b2de0a369b051a37a204fcc
Marco Acero <marco_haz24@hotmail.com>
2022-03-10 09:16:22 -0500

    FIX AREAS

--- src/app/modulos/admin/components/permisos/permisos.component.ts
+++ src/app/modulos/admin/components/permisos/permisos.component.ts
@@ -41,11 +41,12 @@ export class PermisosComponent implements OnInit {
         this.areaService.findAll().then(
             // resp => (<Area[]>resp['data']).forEach(area => this.areaList.push({ label: area.nombre, value: area.id ,title: area.areaPadre.nombre }))
             resp => (<Area[]>resp['data']).forEach(area=>{
-                if(area.areaPadre.nombre == undefined || area.areaPadre.nombre == null){
-                    this.areaList.push({label: area.nombre +" - "+ area.areaPadre.toString(),value : area.id})
-                }else{
-                    this.areaList.push({label: area.nombre  +" - "+ area.areaPadre.nombre,value: area.id})
-                }
+                // if(area.areaPadre.nombre == undefined || area.areaPadre.nombre == null){
+                //     this.areaList.push({label: area.nombre +" - "+ area.areaPadre.toString(),value : area.id})
+                // }else{
+                //     this.areaList.push({label: area.nombre  +" - "+ area.areaPadre.nombre,value: area.id})
+                // }
+                this.areaList.push({label: area.nombre,value : area.id})
             })
         );
         console.log(this.areaList);
