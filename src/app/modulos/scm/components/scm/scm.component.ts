7eb071b098609f34d685dd2e92f40326f26b4ae9
Juanse-92 <juanbernal@lerprevencion.com>
2022-06-23 15:21:14 -0500

    tipoCaso-prioridad

--- src/app/modulos/scm/components/scm/scm.component.ts
+++ src/app/modulos/scm/components/scm/scm.component.ts
@@ -46,6 +46,8 @@ export class ScmComponent implements OnInit {
         'pkUser',
         'sve',
         'eliminado',
+        'prioridadCaso',
+        'tipoCaso',
 
     ];
     estadosList: SelectItem[] = [
