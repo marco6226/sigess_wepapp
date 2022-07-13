05a6a330b769ee3dd3570be8ea1fc8569167b5af
JOHN ALVARO RUEDA FORERO <risgan2727@gmail.com>
2022-07-13 12:28:14 -0500

    Se ajusta vista de documentos por agrupacion

--- src/app/modulos/sec/components/analisis-desviacion/evidencias/evidencias.component.ts
+++ src/app/modulos/sec/components/analisis-desviacion/evidencias/evidencias.component.ts
@@ -21,11 +21,23 @@ export class EvidenciasComponent implements OnInit {
   @Output('onUpdate') onUpdate = new EventEmitter<Documento>();
   @Input('readOnly') readOnly: boolean;
   documentosList: any[];
-  visibleDlg1: boolean;
-  visibleDlg2: boolean;
-  visibleDlg3: boolean;
-  visibleDlg4: boolean;
-  visibleDlg5: boolean;
+  documentosListFoto: any[]=[];
+  documentosListDocumental: any[]=[];
+  documentosListPoliticas: any[]=[];
+  documentosListProcedimientos: any[]=[];
+  documentosListMultimedia: any[]=[];
+  // visibleDlg1: boolean;
+  // visibleDlg2: boolean;
+  // visibleDlg3: boolean;
+  // visibleDlg4: boolean;
+  // visibleDlg5: boolean;
+
+  visibleDlgFoto: boolean;
+  visibleDlgDocumento: boolean;
+  visibleDlgPoliticas: boolean;
+  visibleDlgProcedimientos: boolean;
+  visibleDlgMultimedia: boolean;
+  
   modulo = Modulo.SEC.value;
   msgs: Message[];
 
@@ -36,24 +48,24 @@ export class EvidenciasComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    if (this.documentos != null) {
-      this.documentosList = [];
-       this.documentos.forEach(doc => this.adicionarAGaleria(doc));
-    }
+    // if (this.documentos != null) {
+    //   this.documentosList = [];
+    //    this.documentos.forEach(doc => this.adicionarAGaleria(doc));
+    // }
   }
-  adicionarAGaleria(doc: Documento) {
-    if (this.esImagen(doc)) {
-      this.directorioService.download(doc.id).then(
-        data => {
-          let urlData = this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(<any>data));
-          if(doc.proceso == "politica")
-          this.documentosList.push({ source: urlData, documento: doc });
-          this.documentosList = this.documentosList.slice();
-          console.log(this.documentosList)
-        }
-      );
-    }
-  }
+  // adicionarAGaleria(doc: Documento) {
+  //   if (this.esImagen(doc)) {
+  //     this.directorioService.download(doc.id).then(
+  //       data => {
+  //         let urlData = this.domSanitizer.bypassSecurityTrustUrl(URL.createObjectURL(<any>data));
+  //         if(doc.proceso == "politica")
+  //         this.documentosList.push({ source: urlData, documento: doc });
+  //         this.documentosList = this.documentosList.slice();
+  //         console.log(this.documentosList)
+  //       }
+  //     );
+  //   }
+  // }
 
   esImagen(doc: Documento) {
     let split = doc.nombre.split('.');
@@ -63,27 +75,52 @@ export class EvidenciasComponent implements OnInit {
     return (extension != null && (extension.extension == 'png' || extension.extension == 'jpg'));
   }
 
-  showDialog1() {
-    this.visibleDlg1 = true;
+  showDialog(tipo: string) {
+    console.log("hola");
+    
+    switch (tipo) {
+      case 'foto':
+        this.visibleDlgFoto = true
+        break;
+      case 'documento':
+        this.visibleDlgDocumento = true
+        break;
+      case 'politicas':
+        this.visibleDlgPoliticas = true
+        break;
+      case 'procedimientos':
+        this.visibleDlgProcedimientos = true
+        break;
+      case 'multimedia':
+        this.visibleDlgMultimedia = true
+        break;
+    
+      default:
+        break;
+    }
   }
-  showDialog2() {
-    this.visibleDlg2 = true;
-  }
-  showDialog3() {
-    this.visibleDlg3 = true;
-  }
-  showDialog4() {
-    this.visibleDlg4 = true;
-  }
-  showDialog5() {
-    this.visibleDlg5 = true;
-  }
+
+  // showDialog1() {
+  //   this.visibleDlg1 = true;
+  // }
+  // showDialog2() {
+  //   this.visibleDlg2 = true;
+  // }
+  // showDialog3() {
+  //   this.visibleDlg3 = true;
+  // }
+  // showDialog4() {
+  //   this.visibleDlg4 = true;
+  // }
+  // showDialog5() {
+  //   this.visibleDlg5 = true;
+  // }
 
   onUpload(event: Directorio) {
     if (this.documentos == null)
       this.documentos = [];
     this.documentos.push(event.documento);
-    this.adicionarAGaleria(event.documento);
+    // this.adicionarAGaleria(event.documento);
     this.documentos = this.documentos.slice();
     console.log(this.documentos)
   }
@@ -114,6 +151,44 @@ export class EvidenciasComponent implements OnInit {
         }
       }
     );
+  }
+
+  test(){
+    console.log(this.documentos);
+    this.separador()
+    console.log(this.documentosListFoto,this.documentosListDocumental,this.documentosListMultimedia,this.documentosListPoliticas,this.documentosListProcedimientos);
+    
+  }
+
+  separador(){
+    // setTimeout(() => {
+      this.documentosListFoto=this.documentosListDocumental=this.documentosListMultimedia=this.documentosListPoliticas=this.documentosListProcedimientos =[]
+      this.documentos.forEach(key => {
+        // console.log(key);
+        
+        switch (key.proceso) {
+          case 'fotografica':
+            this.documentosListFoto.push(key)
+            break;
+          case 'documental':
+            this.documentosListDocumental.push(key)
+            break;
+          case 'politica':
+            this.documentosListPoliticas.push(key)
+            break;
+          case 'procedimientos':
+            this.documentosListProcedimientos.push(key)
+            break;
+          case 'multimedia':
+            this.documentosListMultimedia.push(key)
+            break;
+        
+          default:
+            break;
+        }
+      });
+    // }, 5000);
+    
   }
 }
 
