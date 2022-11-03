65f28f956fc21af2ea47de631d780d0b3d440973
Juanse-92 <juanbernal@lerprevencion.com>
2022-11-03 17:15:11 -0500

    homeCorona días perdidos

--- src/app/modulos/core/components/home-corona/home-corona.component.ts
+++ src/app/modulos/core/components/home-corona/home-corona.component.ts
@@ -3,6 +3,9 @@ import { locale_es } from 'app/modulos/rai/enumeraciones/reporte-enumeraciones';
 import { ReporteService } from 'app/modulos/rai/services/reporte.service';
 import { FilterQuery } from "../../../core/entities/filter-query";
 import { AnalisisDesviacionService } from "app/modulos/sec/services/analisis-desviacion.service";
+import {DesviacionService } from "app/modulos/sec/services/desviacion.service";
+import { Criteria } from '../../../core/entities/filter';
+import { SesionService } from '../../../core/services/sesion.service';
 
 @Component({
   selector: 'app-home-corona',
@@ -14,51 +17,70 @@ export class HomeCoronaComponent implements OnInit {
   desde: Date;
   hasta: Date;
   NoEventos:number;
-  diasPerdidos;
+  diasPerdidos:number;
+  incapacidades;
+  areasPermiso: string;
   fieldsR: string[] = [
     'id'
   ];
   fieldsAD: string[] = [
     'incapacidades'
   ];
+  fieldsD: string[] = [
+    'analisisId'
+  ];
   
 
   constructor(
+    private sesionService: SesionService,
     public repService: ReporteService,
     public analisisDesviacionService: AnalisisDesviacionService,
+    public desviacionService: DesviacionService,
   ) { }
 
   async ngOnInit() {
+    this.areasPermiso = await this.sesionService.getPermisosMap()['SEC_GET_DESV'].areas;
     await this.reporte()
-    await this.analisisDesviacion()
+    await this.desviacion()
   }
   async reporte(){
     let fq1 = new FilterQuery();
     fq1.fieldList = this.fieldsR;
     await this.repService.findByFilter(fq1).then((resp)=>{
-      console.log(resp)
       this.NoEventos=resp['data'].length;
     })
   }
 
-  async analisisDesviacion(){
-    this.analisisDesviacionService.findAll().then((resp)=>{
-      console.log(resp)
-    })
+  async analisisDesviacion(event :any){
     let fq2 = new FilterQuery();
-    fq2.fieldList = this.fieldsAD;
-    this.diasPerdidos=[];
+    fq2.fieldList=this.fieldsAD;  
+    fq2.filterList=[{ criteria: Criteria.CONTAINS, field: "id", value1: event }];
     await this.analisisDesviacionService.findByFilter(fq2).then((resp)=>{
-      console.log(resp)
-      // 
-      resp['data'].forEach(element => {
-        // console.log(element)
-        this.diasPerdidos.push(JSON.parse(resp["incapacidades"]));
-      });
-      
-      // this.NoEventos=resp['data'].length;
+      if(resp['data'][0]['incapacidades'])
+      {
+        if(JSON.parse(resp['data'][0]['incapacidades'])!=null){
+          this.diasPerdidos=this.diasPerdidos+JSON.parse(resp['data'][0]['incapacidades']).length
+        }
+      }
     })
-    console.log(this.diasPerdidos)
   }
 
+  async desviacion(){
+
+    let filterQuery = new FilterQuery();
+    filterQuery.fieldList=this.fieldsD;
+    filterQuery.filterList=[{ criteria: Criteria.CONTAINS, field: "area.id", value1: this.areasPermiso }];
+
+    await this.desviacionService.findByFilter(filterQuery).then(
+      resp => {
+        this.diasPerdidos=0;
+        resp['data'].forEach(async element => {
+            if(element['analisisId']){
+              await this.analisisDesviacion(element['analisisId'])
+            }
+          }
+        )       
+      }
+    )
+  }
 }
