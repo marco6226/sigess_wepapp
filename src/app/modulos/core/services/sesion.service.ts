cab5f57fe7fd02d4a71cb87d595cc4d85fdae8f6
Marco Acero <marco_haz24@hotmail.com>
2023-01-12 14:41:00 -0500

    ajuste lectura docuemntos ado

--- src/app/modulos/core/services/sesion.service.ts
+++ src/app/modulos/core/services/sesion.service.ts
@@ -192,6 +192,6 @@ export class SesionService {
         //if (this.app_version == null)
         //this.app_version = localStorage.getItem("app_version");
 
-        return "1.0.241";
+        return "1.0.242";
     }
 }
