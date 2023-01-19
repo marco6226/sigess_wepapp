4cfeb4b826d5a6550e1354e96c52b57d48c86c5c
Juanse-92 <juanbernal@lerprevencion.com>
2023-01-19 16:30:44 -0500

    correción aliados

--- src/app/modulos/ctr/components/aliados-list/aliados-list.component.ts
+++ src/app/modulos/ctr/components/aliados-list/aliados-list.component.ts
@@ -28,7 +28,16 @@ export class AliadosListComponent implements OnInit {
   excel:any[];
   rangeDatesCreacion: any;
   rangeDatesActualizacion: any;
- 
+  es = {
+    firstDayOfWeek: 1,
+    dayNames: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+    dayNamesShort: ["Dom", "Lun", "Mar", "Miér", "Juev", "Vier", "Sáb"],
+    dayNamesMin: ["Do", "Lu", "Ma", "Mi", "Ju", "Vi", "Sá"],
+    monthNames: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+    monthNamesShort: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+    today: 'Hoy',
+    clear: 'Limpiar'
+  };
   
   constructor(
     private empresaService: EmpresaService,
